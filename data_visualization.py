d2805707c1c9d96996858ae70a135f6010011379
Kyle Scagnelli <kjs5107@rit.edu>
2019-02-01 18:31:19 -0500

    Explored data visualization using appropriate libs like pandas and matplotlib

--- data_visualization.py
+++ data_visualization.py
@@ -1,8 +1,31 @@
 import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+
+
+# read in the csv and setup the dataset
+data = './assets/iris_df.csv'
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(data, names=names)
+
+
+def univariate_plots():
+    # box & whisker plots
+    dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+
+    # histogram plots
+    dataset.hist()
+
+
+def multivariate_plots():
+    scatter_matrix(dataset)
 
 
 def main():
-    pass
+    univariate_plots()
+    multivariate_plots()
+    # display all plots
+    plt.show()
 
 
 if __name__ == "__main__":
